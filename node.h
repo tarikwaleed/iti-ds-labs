48862527408cd976690538dd307571fab28dcdd8
tarikwaleed <takizee53@gmail.com>
2022-12-08 21:03:29 +0200

    :hammer: node.h-> :one: Adding include Guards :two: Defining new function Node* CreateNode(int data);

--- node.h
+++ node.h
@@ -1,6 +1,18 @@
-
+#ifndef NODE_H_
+#define NODE_H_
+#include <stdlib.h>
 typedef struct Node {
   int Data;
   struct Node *Prev;
   struct Node *Next;
 } Node;
+Node *CreateNode(int data) {
+  // Allocate the node in the heap
+  Node *newNode = (Node*)malloc(sizeof(Node));
+  // Fill its members
+  newNode->Data = data;
+  newNode->Prev = NULL;
+  newNode->Next = NULL;
+  return newNode;
+}
+#endif
