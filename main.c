433146438858e8e0d1ce1fdbf7b02007796132a6
tarikwaleed <takizee53@gmail.com>
2022-12-09 08:34:26 +0200

    :hammer: main.c -> some changes

--- main.c
+++ main.c
@@ -5,20 +5,25 @@
 #include "stdio.h"
 #include "stdlib.h"
 int main(int argc, char const *argv[]) {
-  const char* operation="LinkedListInsert_tail";
+  const char* operation="LinkdedListGetDataByIndex";
   LinkedList list = {.head = NULL, .tail = NULL,.lenght=0};
+
   LinkedListInsert_tail(&list, 1);
   LinkedListInsert_tail(&list, 2);
   LinkedListInsert_tail(&list, 3);
   LinkedListInsert_tail(&list,4);
   LinkedListInsert_tail(&list,5);
+
   printf("List Before %s operation:\n",operation);
   LinkedListDisplay(&list);
+
   /////////////////////// TEST //////////////////////////
-  // LinkedList(&list,4);
+  int result=LinkedListGetDataByIndex(&list,2);
   /////////////////////// TEST //////////////////////////
-  printf("List After %s operation:\n",operation);
-  LinkedListDisplay(&list);
-  printf("%d",list.lenght);
+  printf("%d\n",result);
+  printf("%d\n",list.head->Data);
+
+  // printf("List After %s operation:\n",operation);
+  // LinkedListDisplay(&list);
   return 0;
 }
